aa07dfeeceeff04a28ef6da02b8ccf1c1600a98d
mikhalexandr <mikhalchenkov22@gmail.com>
2025-11-09 13:34:18 +0300

    fix: lab1

--- lab1/src/ls_hierarchy.sh
+++ lab1/src/ls_hierarchy.sh
@@ -1,1 +1,5 @@
-ls -lR ../lab0 > ../docs/file_hierarchy_after_step_3.txt
+echo -e "-- Save file and directory hierarchy after step 3 to ../docs/file_hierarchy_after_step_3.txt"
+
+ls -lR ../lab0 > ../docs/file_hierarchy_after_step3.txt 2>/dev/null
+
+echo -e "========================"
